d9113573651deefe1a3ce22288ac0bd11fb45160
Robinson Mukena Zevo <romukena@f4r5s1.clusters.42paris.fr>
2025-08-04 17:36:50 +0200

    done

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 15:42:52 by romukena          #+#    #+#             */
-/*   Updated: 2025/08/04 15:58:21 by romukena         ###   ########.fr       */
+/*   Updated: 2025/08/04 17:36:11 by romukena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,39 +37,44 @@ void	close_files_without_eror(int fd1, int fd2, int fd3, int fd4)
 		close(fd4);
 }
 
+void	handle_child1(int fd[2], int pipe_fd[2],
+		char **av, char **envp)
+{
+	close_files_without_eror(-1, fd[1], pipe_fd[0], -1);
+	child_process_1(fd[0], pipe_fd[1], av[2], envp);
+}
+
+void	handle_child2(int fd[2], int pipe_fd[2],
+		char **av, char **envp)
+{
+	close_files_without_eror(-1, fd[0], -1, pipe_fd[1]);
+	child_process_2(pipe_fd[0], fd[1], av[3], envp);
+}
+
+
 void	pipex(char **av, char **envp)
 {
-	int		infile_fd;
-	int		outfile_fd;
+	int		fd[2];
 	int		pipe_fd[2];
 	pid_t	pid;
 	pid_t	pid2;
 
-	infile_fd = open(av[1], O_RDONLY);
-	if (infile_fd == -1)
+	fd[0] = open(av[1], O_RDONLY);
+	fd[1] = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd[0] < 0 || fd[1] < 0 || pipe(pipe_fd) < 0)
 		exit_with_error("infile");
-	outfile_fd = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (outfile_fd == -1)
-		close_files(infile_fd, -1, -1, -1);
-	if (pipe(pipe_fd) == -1)
-		close_files(infile_fd, outfile_fd, -1, -1);
 	pid = fork();
 	if (pid < 0)
-		close_files(infile_fd, outfile_fd, pipe_fd[0], pipe_fd[1]);
+		close_files(fd[0], fd[1], pipe_fd[0], pipe_fd[1]);
 	if (pid == 0)
-	{
-		close_files_without_eror(-1, outfile_fd, pipe_fd[0], -1);
-		child_process_1(infile_fd, pipe_fd[1], av[2], envp);
-	}
+		handle_child1(fd, pipe_fd, av, envp);
 	pid2 = fork();
 	if (pid2 < 0)
-		close_files(infile_fd, outfile_fd, pipe_fd[0], pipe_fd[1]);
+		close_files(fd[0], fd[1], pipe_fd[0], pipe_fd[1]);
 	if (pid2 == 0)
-	{
-		close_files_without_eror(infile_fd, -1, -1, pipe_fd[1]);
-		child_process_2(pipe_fd[0], outfile_fd, av[3], envp);
-	}
-	close_files_without_eror(infile_fd, outfile_fd, pipe_fd[0], pipe_fd[1]);
+		handle_child2(fd, pipe_fd, av, envp);
+	close_files_without_eror(fd[0], fd[1], pipe_fd[0], pipe_fd[1]);
 	waitpid(pid, NULL, 0);
 	waitpid(pid2, NULL, 0);
 }
+
