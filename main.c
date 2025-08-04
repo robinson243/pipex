95ae9e9cbb26431a67c665254453143675d7b68a
Robinson Mukena Zevo <romukena@f4r5s1.clusters.42paris.fr>
2025-08-04 17:50:31 +0200

    done

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 15:41:53 by romukena          #+#    #+#             */
-/*   Updated: 2025/08/04 15:57:54 by romukena         ###   ########.fr       */
+/*   Updated: 2025/08/04 17:43:57 by romukena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	main(int argc, char **argv, char **envp)
 {
-	if (argc != 5)
+	if (argc != 5 || !*envp)
 	{
 		write(2, "Usage: ./pipex infile cmd1 cmd2 outfile\n", 41);
 		return (1);
