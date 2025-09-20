93dca16b62471f54fe9f23dee826bb43dd54488b
Robinson Mukena Zevo <romukena@f1br9s4.clusters.42paris.fr>
2025-09-20 12:52:36 +0200

    modify some stuff

--- files.c
+++ files.c
@@ -6,7 +6,7 @@
 /*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 12:43:09 by romukena          #+#    #+#             */
-/*   Updated: 2025/09/14 16:06:40 by romukena         ###   ########.fr       */
+/*   Updated: 2025/09/20 12:51:17 by romukena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,6 @@
 
 /*➜  pipex git:(main) ✗ valgrind --leak-check=full
 --trace-children=yes --track-fds=yes ./pipex Makefile "sleep 5" "sleep 5" out*/
-
-static char	*print_command_error(char *cmd)
-{
-	char	*sentence;
-	char	*dest;
-
-	sentence = ": command not found\n";
-	dest = ft_strjoin(cmd, sentence);
-	if (!dest)
-		free(dest);
-	return (dest);
-}
 
 void	exit_with_error(char *msg)
 {
@@ -50,16 +38,12 @@ void	child_process_1(int fd[2], int pipe[2], char *cmd, char **envp)
 	char	*pathname;
 	char	**good_path;
 	char	**args;
-	char	*phrase;
 
 	good_path = get_paths(envp);
 	args = parse_cmd(cmd);
 	pathname = find_cmd_path(good_path, args[0]);
 	if (!pathname)
 	{
-		phrase = print_command_error(args[0]);
-		ft_putstr_fd(phrase, 2);
-		free (phrase);
 		close_files_without_eror(fd[0], fd[1], pipe[0], pipe[1]);
 		free_all_and_exit(NULL, good_path, args, 127);
 	}
@@ -75,16 +59,12 @@ void	child_process_2(int fd[2], int pipe[2], char *cmd, char **envp)
 	char	*pathname;
 	char	**good_path;
 	char	**args;
-	char	*phrase;
 
 	args = parse_cmd(cmd);
 	good_path = get_paths(envp);
 	pathname = find_cmd_path(good_path, args[0]);
 	if (!pathname)
 	{
-		phrase = print_command_error(args[0]);
-		ft_putstr_fd(phrase, 2);
-		free(phrase);
 		close_files_without_eror(fd[0], fd[1], pipe[0], pipe[1]);
 		free_all_and_exit(NULL, good_path, args, 127);
 	}
--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 15:42:52 by romukena          #+#    #+#             */
-/*   Updated: 2025/09/14 21:57:07 by romukena         ###   ########.fr       */
+/*   Updated: 2025/09/20 12:51:51 by romukena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,26 +34,26 @@ int	open_multi_files(int fd[2], char **av)
 int	pipex(char **av, char **envp)
 {
 	int		fd[2];
-	int		pipe_fd[2];
+	int		pfd[2];
 	pid_t	pid;
 	int		status;
 
 	if (open_multi_files(fd, av) != 0)
-		close_files_without_eror(fd[0], fd[1], pipe_fd[0], pipe_fd[1]);
-	if (pipe(pipe_fd) < 0)
+		return (close_files_without_eror(fd[0], fd[1], pfd[0], pfd[1]), 0);
+	if (pipe(pfd) < 0)
 		return (perror("pipe"), 1);
 	pid = fork();
 	if (pid < 0)
 		return (perror("fork"), 1);
 	if (pid == 0)
-		child_process_1(fd, pipe_fd, av[2], envp);
+		child_process_1(fd, pfd, av[2], envp);
 	waitpid(pid, &status, 0);
 	pid = fork();
 	if (pid < 0)
 		return (perror("fork"), 1);
 	if (pid == 0)
-		child_process_2(fd, pipe_fd, av[3], envp);
-	close_files_without_eror(fd[0], fd[1], pipe_fd[0], pipe_fd[1]);
+		child_process_2(fd, pfd, av[3], envp);
+	close_files_without_eror(fd[0], fd[1], pfd[0], pfd[1]);
 	waitpid(pid, &status, 0);
 	if (WIFEXITED(status))
 		return (WEXITSTATUS(status));
