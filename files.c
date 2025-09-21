573a0d81a3165615d23374b0a13ad05f2a0ca778
mukena_r <robinsonmukena@gmail.com>
2025-09-21 21:55:38 +0200

    end

--- files.c
+++ files.c
@@ -6,7 +6,7 @@
 /*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 12:43:09 by romukena          #+#    #+#             */
-/*   Updated: 2025/09/20 17:14:33 by romukena         ###   ########.fr       */
+/*   Updated: 2025/09/21 21:52:40 by romukena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,11 +38,7 @@ void	child_process_1(int fd[2], int pipe[2], char *cmd, char **envp)
 	char	*pathname;
 	char	**good_path;
 	char	**args;
-	char	*sleeping[3];
 
-	sleeping[0] = "sleep";
-	sleeping[1] = "0.4";
-	sleeping[2] = NULL;
 	good_path = get_paths(envp);
 	args = parse_cmd(cmd);
 	pathname = find_cmd_path(good_path, args[0]);
@@ -50,7 +46,6 @@ void	child_process_1(int fd[2], int pipe[2], char *cmd, char **envp)
 	{
 		ft_putstr_fd(args[0], 2);
 		ft_putstr_fd(": command not found\n", 2);
-		execve("/bin/sleep", sleeping, envp);
 		close_files_without_eror(fd[0], fd[1], pipe[0], pipe[1]);
 		free_all_and_exit(NULL, good_path, args, 127);
 	}
@@ -66,7 +61,11 @@ void	child_process_2(int fd[2], int pipe[2], char *cmd, char **envp)
 	char	*pathname;
 	char	**good_path;
 	char	**args;
+	char	*sleeping[3];
 
+	sleeping[0] = "sleep";
+	sleeping[1] = "0.4";
+	sleeping[2] = NULL;
 	args = parse_cmd(cmd);
 	good_path = get_paths(envp);
 	pathname = find_cmd_path(good_path, args[0]);
@@ -74,6 +73,7 @@ void	child_process_2(int fd[2], int pipe[2], char *cmd, char **envp)
 	{
 		ft_putstr_fd(args[0], 2);
 		ft_putstr_fd(": command not found\n", 2);
+		execve("/bin/sleep", sleeping, envp);
 		close_files_without_eror(fd[0], fd[1], pipe[0], pipe[1]);
 		free_all_and_exit(NULL, good_path, args, 127);
 	}
